ee6998fdd13c32380a29f71a9e8882d7ef1c81a7
Mateusz <mateusz.gurgul.2003@gmail.com>
2024-02-18 16:52:31 +0100

    Dodanie funkcji get_movie i get_series

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class Film():
+class Movie():
     def __init__(self, title, created_date, genre, play_count):
         self.title = title
         self.created_date = created_date
@@ -11,13 +11,64 @@ class Film():
         return f"{self.title} {self.created_date} {self.genre} {self.play_count}"
     
     def __repr__(self):
-        return f"Film(title={self.title}, created_date={self.created_date}, genre={self.genre}, play_count={self.play_count})"
+        return f"Movie(title={self.title}, created_date={self.created_date}, genre={self.genre}, play_count={self.play_count})"
     
     def play(self):
         self.current_play_count += 1
 
-class Series(Film):
+    def panel(self):
+        result = "--------------------------\n"
+        result += f"{self.title}\n"
+        result += f"{self.created_date} {self.genre} {self.play_count}\n"
+        result += "--------------------------"
+        return result
+
+class Series(Movie):
     def __init__(self, nr_episode, nr_sezon, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.nr_episode = nr_episode
         self.nr_sezon = nr_sezon
+
+    def panel(self):
+        result = "--------------------------\n"
+        result += f"{self.title}\n"
+        result += f"{self.created_date} {self.genre} {self.play_count}\n"
+        result += f"{self.nr_sezon} {self.nr_episode}\n"
+        result += "--------------------------"
+        return result
+
+def schedule():
+    movies_list = []
+
+    for i in range(3):
+        movie = Movie(title='Superman', created_date='2024-12-3', genre='horror', play_count='200')
+        movies_list.append(movie)
+
+    for i in range(3):
+        series = Series(title='Batman', created_date='2024-12-3', genre='horror', play_count='200', nr_episode='E06', nr_sezon='S01')
+        movies_list.append(series)
+        
+    for movie in movies_list:
+        if isinstance(movie, Movie):
+            movie.panel()
+        elif isinstance(movie, Series):
+            movie.panel()
+    
+    return movies_list
+
+def get_movies(schedule_list):
+    return [movie for movie in schedule_list if isinstance(movie, Movie)]
+
+def get_series(schedule_list):
+    return [series for series in schedule_list if isinstance(series, Series)]
+
+schedule_list = schedule()
+movies = get_movies(schedule_list)
+print("Movies: ")
+for movie in movies:
+    print(movie.panel())
+
+series = get_series(schedule_list)
+print("Series: ")
+for serie in series:
+    print(serie.panel())
