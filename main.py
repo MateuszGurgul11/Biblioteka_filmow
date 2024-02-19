ef5bf31c568936332321b6aa9c586eb6ffaac3c3
Mateusz <mateusz.gurgul.2003@gmail.com>
2024-02-19 08:39:03 +0100

    Dodanie funkcji search

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 class Movie():
     def __init__(self, title, created_date, genre, play_count):
         self.title = title
@@ -5,21 +7,19 @@ class Movie():
         self.genre = genre
         self.play_count = play_count
 
-        self.current_play_count = 0
-
     def __str__(self):
-        return f"{self.title} {self.created_date} {self.genre} {self.play_count}"
+        return f"{self.title} ({self.created_date})"
     
     def __repr__(self):
-        return f"Movie(title={self.title}, created_date={self.created_date}, genre={self.genre}, play_count={self.play_count})"
+        return f"Movie(title={self.title}, created_date=({self.created_date}), genre={self.genre}, play_count={self.play_count})"
     
     def play(self):
-        self.current_play_count += 1
+        self.play_count += 1
 
     def panel(self):
         result = "--------------------------\n"
-        result += f"{self.title}\n"
-        result += f"{self.created_date} {self.genre} {self.play_count}\n"
+        result += f"{self.title}"
+        result += f"({self.created_date})\n"
         result += "--------------------------"
         return result
 
@@ -31,8 +31,7 @@ class Series(Movie):
 
     def panel(self):
         result = "--------------------------\n"
-        result += f"{self.title}\n"
-        result += f"{self.created_date} {self.genre} {self.play_count}\n"
+        result += f"{self.title} ({self.created_date})\n"
         result += f"{self.nr_sezon} {self.nr_episode}\n"
         result += "--------------------------"
         return result
@@ -40,29 +39,32 @@ class Series(Movie):
 def schedule():
     movies_list = []
 
-    for i in range(3):
-        movie = Movie(title='Superman', created_date='2024-12-3', genre='horror', play_count='200')
+    for i in range(1):
+        movie = Movie(title='Superman', created_date='2024', genre='horror', play_count=200)
         movies_list.append(movie)
 
-    for i in range(3):
-        series = Series(title='Batman', created_date='2024-12-3', genre='horror', play_count='200', nr_episode='E06', nr_sezon='S01')
+    for i in range(1):
+        series = Series(title='Batman', created_date='2019', genre='horror', play_count=200, nr_episode='E06', nr_sezon='S01')
         movies_list.append(series)
-        
-    for movie in movies_list:
-        if isinstance(movie, Movie):
-            movie.panel()
-        elif isinstance(movie, Series):
-            movie.panel()
     
     return movies_list
 
 def get_movies(schedule_list):
-    return [movie for movie in schedule_list if isinstance(movie, Movie)]
+    return [movie for movie in schedule_list if isinstance(movie, Movie) and not isinstance(movie, Series)]
 
 def get_series(schedule_list):
     return [series for series in schedule_list if isinstance(series, Series)]
 
+def search(schedule_list, get_title):
+    return [movie for movie in schedule_list if movie.title == get_title]
+
+def generate_views(schedule_list):
+    rand = randrange(0, 101, 2)
+    movie.play_count = rand
+    return movie.play_count
+
 schedule_list = schedule()
+schedule_list.sort(key=lambda a : a.title)
 movies = get_movies(schedule_list)
 print("Movies: ")
 for movie in movies:
@@ -72,3 +74,12 @@ series = get_series(schedule_list)
 print("Series: ")
 for serie in series:
     print(serie.panel())
+
+get_title = input("Wpisz tytul: ")
+get_title = get_title.capitalize()
+search_result = search(schedule_list, get_title)
+
+for result in search_result:
+    print(result)
+
+print(generate_views(schedule_list))
